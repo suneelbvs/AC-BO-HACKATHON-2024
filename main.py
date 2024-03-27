0f36c9f26713b306c76e5045708aa01d070a9a20
Curtis Chong <curtischong5@gmail.com>
2024-03-27 15:56:26 -0400

    we can now visualize the results

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from data_loaders.tox21 import Tox21
 from results import Result
 import numpy as np
 
+from visualizers.visualize_model_progress import visualize_hits
+
 if __name__ == "__main__":
     NUM_ACTIVE_LEARNING_LOOPS = 5
     THRESHOLD = 0.02
@@ -22,7 +24,7 @@ if __name__ == "__main__":
     results: [Result] = []
 
     # 3: carry out the active learning loop for N times
-    for i in range(NUM_ACTIVE_LEARNING_LOOPS):
+    for batch_num in range(1, NUM_ACTIVE_LEARNING_LOOPS + 1):
         print(f"active_dataset size: ", len(active_dataset))
         # 3.1 update global parameters needed for acquisition functions
         best_observed_value = loader.y(active_dataset).max()
@@ -44,7 +46,7 @@ if __name__ == "__main__":
         print(f"number of hits: {num_hits}", f"number of positive examples in unseen: {positive_class_count_in_unseen}")
 
         results.append(Result(
-            batch_number=i,
+            batch_number=batch_num,
             num_hits=num_hits,
             positive_class_count_in_unseen=positive_class_count_in_unseen
         ))
@@ -61,6 +63,7 @@ if __name__ == "__main__":
         active_dataset = np.concatenate([active_dataset, top_candidates])
     
     # 4: save everything
+    visualize_hits(results)
     
 
 
