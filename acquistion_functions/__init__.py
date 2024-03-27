aea9088019cc9111a183ad1c3701bab395daa9ac
Luis Walter <walter@thphys.uni-heidelberg.de>
2024-03-27 17:46:32 +0100

    Add PI acquisition function

--- acquistion_functions/__init__.py
+++ acquistion_functions/__init__.py
@@ -1,5 +1,6 @@
 """ A module containing a collection of different acquisition functions for Bayesian optimization. """
 from .expected_improvement import expected_improvement
 from .upper_confidence_bound import upper_confidence_bound
+from .probability_of_improvement import probability_of_improvement
 
 __all__ = ["expected_improvement", "upper_confidence_bound"]
