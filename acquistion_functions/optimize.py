2f29b8156af22c1ed2e7b69b403971ac276b18a5
Curtis Chong <curtischong5@gmail.com>
2024-03-27 18:45:25 -0400

    Fix optimize ii (#13)
    * optimize now properly returns the top k indices that are not used yet
    
    * cleanup

--- acquistion_functions/optimize.py
+++ acquistion_functions/optimize.py
@@ -15,18 +15,29 @@ def optimize(*,
     :param max_num_results: The maximum number of results to return.
     :param mode: The optimization mode, either "max" or "min".
     :param kwargs: Additional keyword arguments for the acquisition function.
-    :return: The optimized acquisition function.
+    :return: The indices of the top candidates (that are not in the active dataset).
     """
+
     acquisition_function_values = acquisition_function(mean, uncertainty, **kwargs)
+
+    # convert the array of values into an array of (value, index)
+    # so when we remove the values that are in the active dataset, we can still keep track of the indices in the original array
+    indices = np.arange(acquisition_function_values.shape[0])
+    values_with_indices = np.column_stack((acquisition_function_values, indices))
+
+    # filter for only indices that are NOT inside the active dataset (so we suggest new candidates)
+    indices_not_in_active_dataset = indices[~np.isin(indices, active_dataset)]
+    values_with_indices = values_with_indices[indices_not_in_active_dataset]
+
     if mode == "max":
-        k_th_largest = np.partition(acquisition_function_values, -max_num_results)[-max_num_results]
-        indices = np.nonzero(acquisition_function_values >= k_th_largest)[0]
+        k_th_largest = np.partition(values_with_indices, -max_num_results, axis=0)[-max_num_results][0]
+        best_rows = values_with_indices[values_with_indices[:, 0] >= k_th_largest]
     elif mode == "min":
-        k_th_largest = np.partition(acquisition_function_values, max_num_results)[max_num_results]
-        indices = np.nonzero(acquisition_function_values <= k_th_largest)[0]
+        k_th_largest = np.partition(values_with_indices, max_num_results, axis=0)[max_num_results][0]
+        best_rows = values_with_indices[values_with_indices[:, 0] <= k_th_largest]
     else:
         raise ValueError(f"Invalid mode: {mode}. Must be either 'max' or 'min'.")
     
-    indices_not_in_active_dataset = indices[~np.isin(indices, active_dataset)] # filter for only indices that are NOT inside the active dataset (so we suggest new candidates)
-    num_candidates = min(max_num_results, len(indices_not_in_active_dataset)) # guards against the case where there are fewer than max_num_results candidates
-    return np.random.choice(indices_not_in_active_dataset, num_candidates, replace=False)
+    indices = best_rows[:, 1]
+    num_candidates = min(max_num_results, len(indices)) # guards against the case where there are fewer than max_num_results candidates
+    return np.random.choice(indices, num_candidates, replace=False)
