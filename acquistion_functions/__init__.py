02fe95efd56faab9fe370ca5ad6b6ad131c8db73
Luis Walter <walter@thphys.uni-heidelberg.de>
2024-03-27 18:31:11 +0100

    Add optimizer to __init__.py

--- acquistion_functions/__init__.py
+++ acquistion_functions/__init__.py
@@ -4,6 +4,7 @@ from .upper_confidence_bound import upper_confidence_bound as upper_confidence_b
 from .probability_of_improvement import probability_of_improvement as probability_of_improvement_acquisition
 from .greedy import greedy as greedy_acquisition
 from .random import random as random_acquisition
+from .optimize import optimize as optimize_acquisition_function
 
 __all__ = [
     "expected_improvement_acquisition",
@@ -11,4 +12,5 @@ __all__ = [
     "probability_of_improvement_acquisition",
     "greedy_acquisition",
     "random_acquisition",
+    "optimize_acquisition_function",
 ]
