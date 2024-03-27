dd37e81b3d2db759c393caee5008b36c03f0d14b
Koustaro <131974441+Koustaro@users.noreply.github.com>
2024-03-27 20:43:37 +0100

    Ames Mordred

--- Mordred.py
+++ Mordred.py
@@ -4,20 +4,23 @@ from rdkit import DataStructs
 import pandas as pd
 from mordred import Calculator, descriptors
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import ast
 
-smi_example = ['CN1C=NC2=C1C(=O)N(C(=O)N2C)C','CCCc1ccncc1C(=O)NC']
-mols = [Chem.MolFromSmiles(smi) for smi in smi_example]
+
 
 
 #################################################################
 Dataset = 'Ames'
+fingerprint = 'Mordred'
 #df_data = pd.read_csv(r'C:\Users\js-ne\Downloads\AMES_dataset.csv')
 df_data = pd.read_csv("/home/student/j_spie17/Hackathon/AMES_dataset.csv")
 
 Raw = False
 Numeric = False
-Scaler = True
-Correlation = True
+Scaler = False
+Correlation = False
+recombine = True
 
 if Raw == True:
 
@@ -57,10 +60,38 @@ if Scaler == True:
     df_scaled = pd.DataFrame()
     # Fit and transform the data using the scaler
     for col in df_numeric.columns:
-        df_scaled[col] = scaler.fit_transform(df_numeric[[col]])
+        l = scaler.fit_transform(df_numeric[[col]]).tolist()
+        df_scaled[col] = [float(inner_list[0]) for inner_list in l]
 
     df_scaled.to_csv(f'/home/student/j_spie17/Hackathon/Mordred/scaled_{Dataset}', index=None)
 
+if Correlation == True:
+    df_scaled = pd.read_csv(f'/home/student/j_spie17/Hackathon/Mordred/scaled_{Dataset}')
 
+    corr_matrix = df_scaled.corr()
+    strong_corr_lst = []
+    rows = corr_matrix.index
+    for i in range(len(corr_matrix)):
+        for j in range(i + 1, len(corr_matrix)):
+            if corr_matrix.iloc[j, i] > 0.8:
+                strong_corr_lst.append(rows[j])
+    strong_corr_lst = set(strong_corr_lst)
+    strong_corr_lst = list(strong_corr_lst)
 
+    df_no_corr = df_scaled.drop(strong_corr_lst, axis=1)
+    df_no_corr.to_csv('jan_mordred_no_corr')
+
+    df_no_corr.to_csv(f'/home/student/j_spie17/Hackathon/Mordred/no_corr_{Dataset}', index=False)
+
+if recombine == True:
+    df_no_corr = pd.read_csv(f'/home/student/j_spie17/Hackathon/Mordred/no_corr_{Dataset}')
+    fp = df_no_corr.values.tolist()
+    df_fp = pd.DataFrame({
+        'Drug_ID': df_data['Drug_ID'].tolist(),
+        'Smile': df_data['Drug'].tolist(),
+        f'fingerprint_{fingerprint}': fp,
+        'Y': df_data['Y'].tolist()
+    })
+
+    df_fp.to_csv(f'/home/student/j_spie17/Hackathon/Fingerprints/{Dataset}_{fingerprint}.csv', index=False)
 print('done')
