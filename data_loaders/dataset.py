2133fc61cfe29984a3bf7860ab3f8776d2ef0504
Curtis Chong <curtischong5@gmail.com>
2024-03-27 14:36:31 -0400

    making main loop work with the acquisition functions (#8)
    * making main loop work with the acquisition functions
    
    * main loop works
    
    * cleanup
    
    * added more print statements
    
    * cleanup

--- data_loaders/dataset.py
+++ data_loaders/dataset.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 # This class will be used to load the dataset and perform feature extraction
 # When we do bayesian optimization, we'll call the dataloader to get the x and y values for each dataset slice
@@ -9,14 +8,15 @@ class DataLoader:
         # You should perform feature extraction and data cleaning here
         pass
 
-    def full_dataset(self) -> np.ndarray:
+    def size(self) -> int:
         pass
 
     # for a given slice of the dataset, we'll return the x values
-    def x(self, dataset_slice: pd.DataFrame) -> np.ndarray:
+    # these slices are indexes of the original dataset. e.g. if you pass in [1, 2, 9], The features at index 1, 2, and 9 will be returned
+    def x(self, dataset_slice: np.ndarray) -> np.ndarray:
         pass
 
     # for a given slice of the dataset, we'll return the y values
-    def y(self, dataset_slice: pd.DataFrame) -> np.ndarray:
+    def y(self, dataset_slice: np.ndarray) -> np.ndarray:
         pass
     
--- main.py
+++ main.py
@@ -1,37 +1,56 @@
+from acquistion_functions import optimize, probability_of_improvement
 from models import XGBoostModel
 from data_loaders.tox21 import Tox21
-
+import numpy as np
 
 if __name__ == "__main__":
     NUM_ACTIVE_LEARNING_LOOPS = 5
+    THRESHOLD = 0.02
+
     # 1: load the fingerprint + label data + set the threshold for the succes metric
-    data_loader = Tox21()
-    print(f"loaded {data_loader.name} dataset")
+    loader = Tox21()
+    initial_dataset_size = loader.size()
+    print(f"loaded {loader.name} dataset. num entries: {initial_dataset_size}")
 
     model = XGBoostModel()
 
-    full_dataset = data_loader.full_dataset()
-
     # 2: initialize the model + initialise the first 100 data points from the dataset
-    initial_dataset_size = full_dataset.shape[0]
-    active_dataset = full_dataset[:initial_dataset_size] # TODO: randomly shuffle the data?
+    active_dataset = np.arange(0, initial_dataset_size//20) # TODO: randomize the indices?
+
+    acquisition_function = probability_of_improvement.probability_of_improvement
 
     # 3: carry out the active learning loop for N times
     for i in range(NUM_ACTIVE_LEARNING_LOOPS):
-        # 3.1 train the surrogate model from model.py on the active learning dataset
-        model.fit(data_loader.x(active_dataset), data_loader.y(active_dataset))
+        print(f"active_dataset size: ", len(active_dataset))
+        # 3.1 update global parameters needed for acquisition functions
+        best_observed_value = loader.y(active_dataset).max()
 
-        # 3.2 run the surrogate model over the entire dataset and get predicted values for endpoints
-        mean, uncertainty = model.predict(data_loader.x(full_dataset))
-        print(mean, uncertainty)
-    
-        # 3.3. feed the predicted values into the acquisition function
-    
-        # 3.4 get the top 100 candidates from the acquisition function and add them to the active learning dataset
-    
-        # 3.5 compute the success metric (number of 'hits', positive examples that are above the threshold (or top 10% of entire dataset))
+        # 3.2 train the surrogate model from model.py on the active learning dataset
+        model.fit(loader.x(active_dataset), loader.y(active_dataset))
 
-        # 3.6 repeat the loop
+        # 3.3 run the surrogate model over the unseen dataset and get predicted values for endpoints
+        unseen_data = np.setdiff1d(np.arange(0, initial_dataset_size), active_dataset)
+        mean, uncertainty = model.predict(loader.x(unseen_data))
+
+        # 3.4 compute the success metric (number of 'hits', positive examples that are above the threshold (or top 10% of entire dataset))K
+        is_classified_positive = mean >= THRESHOLD
+        unseen_data_y = loader.y(unseen_data)
+        is_hit = is_classified_positive & (unseen_data_y == 1)
+
+        num_hits = is_hit.sum()
+        positive_class_count = loader.y(unseen_data).sum()
+        print(f"number of hits: {num_hits}", f"number of positive examples in unseen: {positive_class_count}")
+
+    
+        # 3.5. get the top 100 candidates from the acquisition function and add them to the active learning dataset
+        top_candidates = optimize.optimize(acquisition_function,
+                                               mean,
+                                               uncertainty,
+                                               n_results=100,
+                                               best_observed_value=best_observed_value)
+
+        # 3.6 update the active learning dataset
+        active_dataset = np.concatenate([active_dataset, top_candidates])
     
     # 4: save everything
     
