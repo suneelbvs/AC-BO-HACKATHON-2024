139419fb9a9fb91b8f7855726e571a48716673c1
Luis Walter <walter@thphys.uni-heidelberg.de>
2024-03-28 13:58:16 +0100

    Improve acquisition function

--- acquistion_functions/optimize.py
+++ acquistion_functions/optimize.py
@@ -3,15 +3,14 @@ from typing import Callable
 import numpy as np
 
 def optimize(*,
-             acquisition_function: Callable, mean: np.ndarray,
-             uncertainty: np.ndarray, active_dataset: np.ndarray,
-             max_num_results: 100, mode: str = "max", **kwargs) -> np.ndarray:
+    acquisition_function: Callable, mean: np.ndarray,
+    uncertainty: np.ndarray, max_num_results: 100, mode: str = "max", **kwargs
+) -> np.ndarray:
     """
     Optimize the acquisition function.
     :param acquisition_function: The acquisition function to optimize.
     :param mean: Mean of the surrogate model predictions.
     :param uncertainty: Uncertainty of the surrogate model predictions.
-    :param active_dataset: the indices of the unseen data.
     :param max_num_results: The maximum number of results to return.
     :param mode: The optimization mode, either "max" or "min".
     :param kwargs: Additional keyword arguments for the acquisition function.
@@ -20,24 +19,16 @@ def optimize(*,
 
     acquisition_function_values = acquisition_function(mean, uncertainty, **kwargs)
 
-    # convert the array of values into an array of (value, index)
-    # so when we remove the values that are in the active dataset, we can still keep track of the indices in the original array
-    indices = np.arange(acquisition_function_values.shape[0])
-    values_with_indices = np.column_stack((acquisition_function_values, indices))
-
-    # filter for only indices that are NOT inside the active dataset (so we suggest new candidates)
-    indices_not_in_active_dataset = indices[~np.isin(indices, active_dataset)]
-    values_with_indices = values_with_indices[indices_not_in_active_dataset]
-
     if mode == "max":
-        k_th_largest = np.partition(values_with_indices, -max_num_results, axis=0)[-max_num_results][0]
-        best_rows = values_with_indices[values_with_indices[:, 0] >= k_th_largest]
+        k_th_largest = np.partition(
+            acquisition_function_values, -max_num_results, axis=0)[-max_num_results]
+        indices = np.nonzero(acquisition_function_values >= k_th_largest)[0]
     elif mode == "min":
-        k_th_largest = np.partition(values_with_indices, max_num_results, axis=0)[max_num_results][0]
-        best_rows = values_with_indices[values_with_indices[:, 0] <= k_th_largest]
+        k_th_smallest = np.partition(
+            acquisition_function_values, max_num_results, axis=0)[max_num_results]
+        indices = np.nonzero(acquisition_function_values <= k_th_smallest)[0]
     else:
         raise ValueError(f"Invalid mode: {mode}. Must be either 'max' or 'min'.")
-    
-    indices = best_rows[:, 1].astype(np.int64) # since the row was a float dtype (cause we concatenated with the values_with_indices), we need to convert the indices back to int
-    num_candidates = min(max_num_results, len(indices)) # guards against the case where there are fewer than max_num_results candidates
+
+    num_candidates = min(max_num_results, len(indices))
     return np.random.choice(indices, num_candidates, replace=False)
