e2557705686ada8c1f79bdb2258869b7347f95bd
Luis Walter <walter@thphys.uni-heidelberg.de>
2024-03-28 14:10:31 +0100

    Make visualization compatible with fingerprint info

--- visualize.py
+++ visualize.py
@@ -11,21 +11,26 @@ with open('results.csv', 'r', encoding='utf-8') as input_file:
         if row[1] not in data[row[0]]:
             data[row[0]][row[1]] = {}
         if row[2] not in data[row[0]][row[1]]:
-            data[row[0]][row[1]][row[2]] = []
-        data[row[0]][row[1]][row[2]].append(list(map(int, row[3:])))
+            data[row[0]][row[1]][row[2]] = {}
+        if row[3] not in data[row[0]][row[1]][row[2]]:
+            data[row[0]][row[1]][row[2]][row[3]] = []
+        data[row[0]][row[1]][row[2]][row[3]].append(list(map(int, row[3:])))
 os.makedirs('output', exist_ok=True)
 for dataset, dataset_values in data.items():
     for model, model_values in dataset_values.items():
-        plt.figure()
-        for acquisition_function, acquisition_function_values in model_values.items():
-            results = np.array(acquisition_function_values)
-            x = np.arange(results.shape[1])
-            plt.plot(x, results.mean(axis=0), label=acquisition_function)
-            plt.errorbar(x, results.mean(axis=0), yerr=results.std(axis=0), alpha=0.3)
-        plt.grid(alpha=0.3)
-        plt.title(f'{dataset} - {model}')
-        plt.xlabel('Batch number')
-        plt.ylabel('Number of active compounds')
-        plt.legend()
-        plt.savefig(f'output/{dataset}_{model}.png')
-        plt.close()
+        for fingerprint, fingerprint_values in model_values.items():
+            plt.figure()
+            for acquisition_function, acquisition_function_values in fingerprint_values.items():
+                results = np.array(acquisition_function_values)
+                x = np.arange(results.shape[1])
+                slope = np.diff(results.mean(axis=0)).mean()
+                plt.plot(x, results.mean(axis=0), label=f'{acquisition_function} (Slope: {slope:.1f})')
+                #plt.plot(x, results.mean(axis=0), label=acquisition_function)
+                plt.errorbar(x, results.mean(axis=0), yerr=results.std(axis=0), alpha=0.3)
+            plt.grid(alpha=0.3)
+            plt.title(f'{dataset} - {model} - {fingerprint}')
+            plt.xlabel('Batch number')
+            plt.ylabel('Number of active compounds')
+            plt.legend()
+            plt.savefig(f'output/{dataset}_{model}_{fingerprint}.png')
+            plt.close()
